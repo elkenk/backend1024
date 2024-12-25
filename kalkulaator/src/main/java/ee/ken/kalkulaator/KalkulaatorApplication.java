aefbfd8292bbd2c5f7344caa3b14a5f32b08a7c9
elkenk <ken.elken1@gmail.com>
2024-12-25 11:01:04 +0200

    kalkulaator

---
+++ kalkulaator/src/main/java/ee/ken/kalkulaator/KalkulaatorApplication.java
@@ -0,0 +1,13 @@
+package ee.ken.kalkulaator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalkulaatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalkulaatorApplication.class, args);
+	}
+
+}
